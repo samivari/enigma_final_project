7a533fa0f621674c9aceb605a45980aef7cf31da
Sam Iavri <sammi80134@gmail.com>
2022-01-14 11:26:52 -0700

    Key offset made with spec

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,5 +1,16 @@
 class KeyGenerator
+  attr_reader :key
+
   def initialize(key = random_num)
     @key = key
+  end
+
+  def key_offset
+    {
+      A: key[0..1],
+      B: key[1..2],
+      C: key[2..3],
+      D: key[3..4]
+    }
   end
 end
--- spec/key_generator_spec.rb
+++ spec/key_generator_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe 'KeyGenerator' do
     key_generator = KeyGenerator.new('09845')
     expect(key_generator).to be_a(KeyGenerator)
   end
+
+  it 'has key offset' do
+    key_generator = KeyGenerator.new('09845')
+    expect(key_generator.key_offset[:A]).to eq('09')
+    expect(key_generator.key_offset[:B]).to eq('98')
+    expect(key_generator.key_offset[:C]).to eq('84')
+    expect(key_generator.key_offset[:D]).to eq('45')
+  end
 end
