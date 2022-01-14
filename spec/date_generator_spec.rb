f5bb1c85fde8fcf2d1194ee73a364782e67787b7
Sam Iavri <sammi80134@gmail.com>
2022-01-14 11:25:03 -0700

    removing old files.

--- spec/date_generator_spec.rb
+++ spec/date_generator_spec.rb
@@ -6,26 +6,4 @@ RSpec.describe 'DateGenerator' do
     date_generator = DateGenerator.new
     expect(date_generator).to be_a(DateGenerator)
   end
-
-  describe '.generate' do
-    it 'generates a date' do
-      date_generator = DateGenerator
-      Timecop.travel('211111')
-      actual = date_generator.generate
-
-      expected = '111121'
-
-      expect(actual).to eq(expected)
-    end
-
-    it 'generates a different date' do
-      date_generator = DateGenerator
-      Timecop.travel('950208')
-      actual = date_generator.generate
-
-      expected = '080295'
-
-      expect(actual).to eq(expected)
-    end
-  end
 end
