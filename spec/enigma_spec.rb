ae9482092e1ec53b1171a522b8eeca9250b5dcbc
Sam Iavri <sammi80134@gmail.com>
2022-01-15 10:55:57 -0700

    started enigma class with spec

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,9 +1,17 @@
-require 'date'
 require './lib/enigma'
 
 RSpec.describe 'Enigma' do
   it 'exists' do
     enigma = Enigma.new
     expect(enigma).to be_a(Enigma)
+  end
+
+  it 'can encrypt' do
+    enigma = Enigma.new
+    expect(enigma.encrypt(
+             'hello world', '02715', '040895'
+           )).to eq({
+                      encryption: 'keder ohulw', key: '02715', date: '040895'
+                    })
   end
 end
