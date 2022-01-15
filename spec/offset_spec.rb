ceb97bc16af303063d30e4aa6272b33727141abf
Sam Iavri <sammi80134@gmail.com>
2022-01-15 13:53:14 -0700

    Offset can square a date and take the last four digits creating the offset for key

--- spec/offset_spec.rb
+++ spec/offset_spec.rb
@@ -1,8 +1,27 @@
 require './lib/offset'
+require 'timecop'
 
 RSpec.describe 'offset' do
   it 'exists' do
     offset = Offset.new
     expect(offset).to be_a(Offset)
+  end
+
+  it 'can reformat the date' do
+    date = Timecop.travel('220113')
+    offset = Offset.new(date)
+    expect(offset.todays_date).to eq('130122')
+  end
+
+  it 'squares the date' do
+    date = Timecop.travel('220113')
+    offset = Offset.new(date)
+    expect(offset.squared).to eq('16931734884')
+  end
+
+  it 'takes the last four digits from the squared number' do
+    date = Timecop.travel('220113')
+    offset = Offset.new(date)
+    expect(offset.last_four).to eq('4884')
   end
 end
