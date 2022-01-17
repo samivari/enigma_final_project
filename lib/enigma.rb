38a0f85b205d2973c933a990cf028540213e8623
Sam Iavri <sammi80134@gmail.com>
2022-01-16 18:13:48 -0700

    Enigma has a decrypt class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,13 +1,23 @@
 require './lib/offset'
 
 class Enigma
-  def encrypt(message, key = random_num, date = Time.now.strftime('%d%m%y'))
+  def encrypt(message, key = rand(10_000..99_999).to_s, date = Time.now.strftime('%d%m%y'))
     shift = Shift.new(key, date)
 
     encode = Encode.new(message, shift.shifts)
 
     {
       encryption: encode.encode,
+      key: key,
+      date: date
+    }
+  end
+
+  def decrypt(encryption, key = rand(10_000..99_999).to_s, date = Time.now.strftime('%d%m%y'))
+    shift = Shift.new(key, date)
+    decode = Decode.new(encryption, shift.shifts)
+    {
+      decryption: decode.decode,
       key: key,
       date: date
     }
